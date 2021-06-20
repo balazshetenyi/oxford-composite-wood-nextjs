d5f40a88c290583f49237d02e5b114f11e28815c
balazshetenyi <balazshetenyi82@gmail.com>
2021-06-20 17:06:33 +0100

    fixed filtering for accessories

--- utils/filterAccessories.js
+++ utils/filterAccessories.js
@@ -1,51 +1,48 @@
-export function filterAccessories(list, product, products) {
-	const accessories = products.filter((collection) =>
-		collection.title.toLowerCase().includes("accessories")
-	)[0].products
+export function filterAccessories(list, product, collection) {
+	const accessories = collection.products
 
 	if (product.title.toLowerCase().includes("decking")) {
 		const productsToBuy = [
 			{
-				product: accessories.filter((product) => product.title.includes("clip"))[0],
+				product: accessories?.filter((product) => product.title.includes("clip"))[0],
 				quantity: list.clip,
 			},
 			{
-				product: accessories.filter((product) => product.title.includes("fasteners"))[0], // only the first one shows
+				product: accessories?.filter((product) => product.title.includes("fasteners"))[0], // only the first one shows
 				quantity: list.fasteners,
 			},
 			{
-				product: accessories.filter((product) => product.title.includes("joist"))[0],
+				product: accessories?.filter((product) => product.title.includes("joist"))[0],
 				quantity: list.joist,
 			},
-			// {
-			// 	product: accessories
-			// 		.filter((product) => product.title.includes("trim") && product.availableForSale)
-			// 		.filter(
-			// 			(trim) =>
-			// 				trim.variants[0].title.toLowerCase() ===
-			// 				product.variants[0].title.toLowerCase()
-			// 		)[0],
-			// 	quantity: list.trim,
-			// },
+			{
+				product: accessories?.filter(
+					(prod) =>
+						prod.title.includes("trim") &&
+						prod.availableForSale &&
+						prod.options[0].values[0].value === product.options[0].values[0].value
+				)[0],
+				quantity: list.trim,
+			},
 		]
 		return productsToBuy
 	} else if (product.title.toLowerCase().includes("cladding")) {
 		const productsToBuy = [
 			{
-				product: accessories.filter((product) => product.title.includes("clip"))[0],
+				product: accessories?.filter((product) => product.title.includes("clip"))[0],
 				quantity: list.clip,
 			},
 			{
-				product: accessories.filter((product) => product.title.includes("joist"))[0],
+				product: accessories?.filter((product) => product.title.includes("joist"))[0],
 				quantity: list.joist,
 			},
 			{
-				product: accessories
-					.filter((product) => product.title.includes("trim"))
-					.filter(
-						(trim) =>
-							trim.options[0].values[0].value === product.options[0].values[0].value
-					)[0],
+				product: accessories?.filter(
+					(prod) =>
+						prod.title.includes("trim") &&
+						prod.availableForSale &&
+						prod.options[0].values[0].value === product.options[0].values[0].value
+				)[0],
 				quantity: list.trim,
 			},
 		]
